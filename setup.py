813232a0cde793bb7e7e4ac2d38973fbf4b45880
Paul Jena <paul.jena@optimeas.com>
2022-09-12 23:47:46 +0200

    Fix setup.py to use setuptools and proper name

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@
 #  MA 02110-1301, USA.
  #
 
-from distutils.core import setup
+from setuptools import setup
 
 
-setup(name='KD3005P Library',
-        version='0.1',
+setup(name='kd3005p',
+        version='0.0.1',
         py_modules=['kd3005p'])
